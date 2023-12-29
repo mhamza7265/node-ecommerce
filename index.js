d8422f0b29848dee15a88fa252589b05de21d336
Muhammad Hamza <m.hamza7265@gmail.com>
2023-12-29 19:32:06 +0500

    new

--- index.js
+++ index.js
@@ -73,3 +73,5 @@ app.delete("/product/:id", deleteSingleProduct);
 app.put("/product/:id", upload.any(), updateProduct);
 
 app.listen("3000", () => console.log("Server started on port 3000"));
+
+module.exports = app;
