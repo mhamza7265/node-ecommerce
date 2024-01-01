8cb0940a66cae3a4b1289aaa98fd6bd121fbb301
Muhammad Hamza <m.hamza7265@gmail.com>
2024-01-01 14:40:48 +0500

    mongo uri updated

--- config/connectToDB.js
+++ config/connectToDB.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const {MongoClient, ServerApiVersion} = require("mongodb");
 
 const connectToDB = async() => {
     try{
-        await mongoose.connect("mongodb://localhost:27017/studentsData", { family: 4 });
+        await mongoose.connect("mongodb+srv://mhamza7265:42mongo68@cluster0.5nu3crx.mongodb.net/node-ecommerce?retryWrites=true&w=majority", { family: 4});
         console.log("Connected to DB")
     }catch(err){
         console.log(err)
